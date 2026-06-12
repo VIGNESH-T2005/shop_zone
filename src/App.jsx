351113afc8a240da91a0643f47cdaaa9e2cb0f0e
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-12 19:26:53 +0530

    update routnig

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import ProductDetail from './pages/ProductDetail'
+import CartPage from './pages/CartPage'
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
