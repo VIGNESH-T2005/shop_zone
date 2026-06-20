ab2f138611b0afb406e8262cf8a430ee2e94a2fd
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-20 20:02:59 +0530

    adding backend deploy link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://shop-zone-backend-2zbq.onrender.com/',
+  baseURL: 'https://shop-zone-backend-2zbq.onrender.com/api',
 })
 
 export const getProducts = () => API.get('/products')
